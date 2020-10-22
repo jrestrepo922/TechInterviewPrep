18a84f80a9640565d541e347236247c524fd2a84
jrestrepo922 <tinto922@gmail.com>
2020-10-22 11:16:50 -0400

    implemented a merge and quick sort

--- index.js
+++ index.js
@@ -85,12 +85,12 @@ class ArrayList{
 /**********************************
 * CREATE AND REVERSE A LINKEND LIST
 ***********************************/
-class Node{
-    constructor(value){
-        this.value = value; 
-        this.next = null; 
-    }
-}
+// class Node{
+//     constructor(value){
+//         this.value = value; 
+//         this.next = null; 
+//     }
+// }
 
 class SinglyLinkedList{
     constructor(value){
@@ -289,6 +289,85 @@ class Hash{
 * CREATE A GRAPH USING AN ADJANCENCY LIST AND WRITE BFS AND DFS
 ***************************************************************/
 
+class Graph{
+    constructor(){
+        this.adjencyList = {};
+        this.length = 0;
+    }
+
+    addVertex(value){
+        this.adjencyList[value] = []; 
+        this.length++;
+        return this.length;
+    }
+
+    addEdges(vertex1, vertex2){
+        if(!this.adjencyList[vertex2] || !this.adjencyList[vertex1]) return false; 
+        this.adjencyList[vertex1].push(vertex2);
+        this.adjencyList[vertex2].push(vertex1);
+    }
+
+    // TC: O(N^2)
+    // SC: O(N)
+    BFS(vertex){
+        if(!this.adjencyList[vertex]) return false; 
+        const list = [];
+        const queue = []; 
+        const visited = {}; 
+        let removedVertex; 
+        queue.push(vertex); 
+        while(queue.length !== 0){
+            removedVertex = queue.shift();
+            if(!visited[removedVertex]){
+                list.push(removedVertex);
+                visited[removedVertex] = true; 
+            }
+            this.adjencyList[removedVertex].forEach(neighbor =>{
+                if(!visited[neighbor]){
+                    queue.push(neighbor)
+                }
+            })  
+        }
+        return list; 
+    }
+
+
+    DFS(vertex){
+        const list = []; 
+        const visited =  {}; 
+        const self = this;
+        function DFSHelper(currentVertex){
+            if(!self.adjencyList[currentVertex]) return false; 
+            if(!visited[currentVertex]){
+                list.push(currentVertex);
+                visited[currentVertex] = true; 
+                self.adjencyList[currentVertex].forEach(neighbor => {
+                    if(!visited[neighbor]){
+                        DFSHelper(neighbor);
+                    }
+                })
+            }
+        }
+        DFSHelper(vertex);
+        return list; 
+
+    }
+
+
+}
+
+// let myGraph = new Graph();
+// myGraph.addVertex(1);
+// myGraph.addVertex(2);
+// myGraph.addVertex(3);
+// myGraph.addVertex(4);
+// myGraph.addVertex(5);
+// myGraph.addEdges(1, 2);
+// myGraph.addEdges(1, 3);
+// myGraph.addEdges(2, 4);
+// myGraph.addEdges(3, 5);
+// myGraph.addEdges(4, 5);
+// console.log(myGraph.DFS(1))
 
 
 
@@ -325,22 +404,220 @@ function binarySearch(sortedArray, target){
 /******************
 * WRITE A MERGESORT 
 *******************/
+// we need a merge function that combines two sorted arrays together 
+function merge(arr1, arr2){
+    // make sure the values pass are arrays
+    // make sure the values are not undefined 
+    // make sure the values are all integers; 
+    const mergeArr = []; 
+    let i = 0;
+    let j = 0; 
+    while(i < arr1.length && j < arr2.length){  
+        if(arr1[i] < arr2[j]){
+            mergeArr.push(arr1[i])
+            i++
+        }else{
+            mergeArr.push(arr2[j])
+            j++
+        }
+    }
+    while(i < arr1.length){
+        mergeArr.push(arr1[i]);
+        i++
+    }
+    while(j < arr2.length){
+        mergeArr.push(arr2[j])
+        j++
+    }
+    return mergeArr;
+}
 
+// console.log(merge([1,3,5], [2,4,6,7,8]))
 
+function mergeSort(arr){
+    if(arr.length === 1) return arr; 
+    let middle = Math.floor(arr.length/ 2)
+    let left = mergeSort(arr.slice(0, middle));
+    let rigth = mergeSort(arr.slice(middle));
+    return merge(left, rigth);
+}
 
-
-
+// console.log(mergeSort([3,2,1,4, 8 , 11, 90 , 40]))
 
 /******************
 * WRITE A QUICKSORT
 *******************/
+// create a pivot function
+function pivot(arr, start = 0, end = arr.length - 1){
+    let swapIndex = start; 
+    const pivot = arr[start];
+    for(let i = start + 1; i <= end; i++){
+        if(pivot > arr[i]){
+            swapIndex++;
+            swap(arr, swapIndex, i)
+        }
+    }
+    swap(arr, swapIndex, start);
+    return swapIndex; 
+}
 
+// console.log(pivot([4,8,2,1,5]))
+// create a swap function 
+function swap(arr, index1, index2){
+    const temp = arr[index1]; 
+    arr[index1] = arr[index2]; 
+    arr[index2] = temp;
+}
 
+//create quickSort
+function quickSort(arr, left = 0, right = arr.length - 1){
+    if(left < right){
+        let pivotIndex = pivot(arr, left, right )// 2
+        // left
+        quickSort(arr,left, pivotIndex -1);
+        // right
+        quickSort(arr, pivotIndex + 1, right); 
+    }
+    return arr; 
+}
+
+console.log(quickSort([4,8,2,1,5]))
 
 /***************************************************************
 * PRINT BINARY TREE USING DFS(INORDER, PREORDER, POSTORDER) AND BFS
 ***************************************************************/
+class Node{
+    constructor(value){
+        this.value = value, 
+        this.left = null, 
+        this.right = null 
+    }
+}
 
+
+
+
+
+
+
+
+
+class BST{
+    constructor(value){
+        this.root = new Node(value)
+    }
+
+    insert(value){
+        if(typeof value !== "number" || value < 0) return; 
+        // create a variable that holds the currentNode. You will always start with the root
+        let currentNode = this.root; 
+        // we want to insert once we get to a value and its right or left property is null.
+        const newNode = new Node(value)
+        while(true){
+            // need to check if greater or smaller than the currentNode.value
+            if(value < currentNode.value ){
+                if(currentNode.left === null){
+                    currentNode.left = newNode;
+                    return this; 
+                }
+                currentNode = currentNode.left; 
+            } else {
+                if(currentNode.right === null){
+                    currentNode.right = newNode; 
+                    return this; 
+                }
+                currentNode = currentNode.right;
+            }
+        } 
+
+
+    }
+
+    lookUp(value){
+        if(typeof value !== "number" || value < 0) return false; 
+        let currentNode = this.root; 
+        while(currentNode !== null){
+            if(value < currentNode.value){
+                currentNode = currentNode.left;
+            } else if (value > currentNode.value){
+                currentNode = currentNode.right;
+            } else {
+                return currentNode; 
+            }
+        }
+        return false; 
+    }
+
+    BFS(){
+        const list = []; 
+        const queue = []; 
+        let currentNode = this.root; 
+        queue.push(currentNode);
+        let removedNode; 
+        while(queue.length !== 0){
+            removedNode = queue.shift();
+            list.push(removedNode.value);
+            if(removedNode.left){
+                queue.push(removedNode.left);
+            }
+            if(removedNode.right){
+                queue.push(removedNode.right)
+            }
+        }
+        return list; 
+    }
+
+    DFSPreOrder(){
+        let currentNode = this.root; 
+        const list = []; 
+        function DFSHelper(currentNode){
+            if(currentNode === null) return;
+            list.push(currentNode.value);
+            DFSHelper(currentNode.left);
+            DFSHelper(currentNode.right)
+        }
+        DFSHelper(currentNode);
+        return list;
+    }
+
+    DFSInOrder(){
+        let currentNode = this.root; 
+        const list = []; 
+        function DFSHelper(currentNode){
+            if(currentNode === null) return;
+            DFSHelper(currentNode.left);
+            list.push(currentNode.value);
+            DFSHelper(currentNode.right)
+        }
+        DFSHelper(currentNode);
+        return list;
+    }
+
+    DFSPostOrder(){
+        let currentNode = this.root; 
+        const list = []; 
+        function DFSHelper(currentNode){
+            if(currentNode === null) return;
+            DFSHelper(currentNode.left);
+            
+            DFSHelper(currentNode.right);
+            list.push(currentNode.value);
+        }
+        DFSHelper(currentNode);
+        return list;
+    }
+
+    
+}
+
+// let myBST = new BST(4);
+// myBST.insert(3);
+// myBST.insert(2);
+// myBST.insert(3);
+// myBST.insert(8);
+// myBST.insert(7);
+// console.log(myBST)
+// console.log(myBST.DFSPostOrder());
 
 
 
